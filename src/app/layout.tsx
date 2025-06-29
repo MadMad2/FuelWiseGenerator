b51e35443c60f1ec299a84198d97198fa5a092d9
Dmitro Bliskavka <dmtry.bliskavka@gmail.com>
2025-06-29 14:10:48 +0000

    как добавить чтоб эта иконка отображалась на ярлыке сохраненной страничк

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
     <html lang="uk" className="dark">
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"></link>
